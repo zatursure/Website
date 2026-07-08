20824a1af0d051d3ef3ef78fc01581a40c23e4ee
Zatursure <zatursure@zako.net.cn>
2026-07-08 17:07:53 +0800

    fix: update music playlist ID and API endpoint

--- themes/anzhiyu/scripts/events/merge_config.js
+++ themes/anzhiyu/scripts/events/merge_config.js
@@ -571,9 +571,9 @@ hexo.extend.filter.register(
       nav_music: {
         enable: true,
         console_widescreen_music: false,
-        id: 8152976493,
+        id: 9426418004,
         server: "netease",
-        all_playlist: "https://y.qq.com/n/ryqq/playlist/8802438608",
+        all_playlist: "https://music.163.com/#/playlist?id=9426418004",
       },
       visitorMail: {
         enable: true,
